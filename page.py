6e68f6d957a711e2ed595b5886f71462b4786fe5
daviis <daviis01@luther.edu>
2014-04-26 20:18:52 -0500

    attempted to login stuff but didn't complete

---
+++ page.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, session, abort, redirect, url_for
+app = Flask(__name__)
+app.secret_key = "heartbleed"
+
+@app.route('/')
+def index():
+	return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	error = None
+	if request.method == 'POST':
+		if valid_login(request.form['username']), request.form['password']:
+		return logTheUserIn(request.form['username'])
+	else:
+		abort(401)	
+	return render_template('login.html')
+
+if __name__ == '__main__':
+	app.debug = True
+	app.run()	
